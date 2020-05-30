ce9016e159862405fd5cd57bc4e1a4a37cbb1b5c
Dmytro Pashko <pashko.dmytro@gmail.com>
2020-05-31 02:08:01 +0300

    Fixed issue with multiple allocations.

--- app/src/main/java/com/dpashko/ui/main/MainActivity.kt
+++ app/src/main/java/com/dpashko/ui/main/MainActivity.kt
@@ -1,23 +1,21 @@
 package com.dpashko.ui.main
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.dpashko.insets.R
-import dagger.android.AndroidInjection
+import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity(R.layout.main_activity) {
 
     @Inject
     lateinit var factory: ViewModelProvider.Factory
     private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
         viewModel = ViewModelProvider(this, factory).get(MainActivityViewModel::class.java)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
--- app/src/main/java/com/dpashko/ui/main/MainFragment.kt
+++ app/src/main/java/com/dpashko/ui/main/MainFragment.kt
@@ -1,13 +1,12 @@
 package com.dpashko.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.dpashko.insets.R
-import dagger.android.support.AndroidSupportInjection
+import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
-class MainFragment : Fragment(R.layout.main_fragment) {
+class MainFragment : DaggerFragment(R.layout.main_fragment) {
 
     companion object {
         fun newInstance() = MainFragment()
@@ -16,11 +15,6 @@ class MainFragment : Fragment(R.layout.main_fragment) {
     @Inject
     lateinit var factory: ViewModelProvider.Factory
     private lateinit var viewModel: MainFragmentViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidSupportInjection.inject(this)
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
